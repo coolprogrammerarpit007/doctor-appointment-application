38b35cec0f3e8fe8f3a8bf63c4c5abf532c9159f
Arpit <arpit.mishra@microprixs.in>
2026-01-09 13:17:16 +0530

    spatie roles managment

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    public function run(): void
+    {
+        // Reset cached roles and permissions
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'doctor']);
+    }
+}
